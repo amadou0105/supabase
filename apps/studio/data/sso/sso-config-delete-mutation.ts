f4f250a0d718e0da410aa75863d01782210f5aee
Chris Stockton <chris.stockton@supabase.io>
2026-03-12 09:14:25 -0700

    chore: invalidate org members on sso provider deletion

--- apps/studio/data/sso/sso-config-delete-mutation.ts
+++ apps/studio/data/sso/sso-config-delete-mutation.ts
@@ -2,6 +2,7 @@ import { useMutation, useQueryClient } from '@tanstack/react-query'
 import { toast } from 'sonner'
 
 import { handleError, del } from 'data/fetchers'
+import { organizationKeys as organizationKeysV1 } from 'data/organizations/keys'
 import type { ResponseError, UseCustomMutationOptions } from 'types'
 import { orgSSOKeys } from './keys'
 
@@ -34,7 +35,10 @@ export const useSSOConfigDeleteMutation = ({
     mutationFn: (vars) => deleteSSOConfig(vars),
     async onSuccess(data, variables, context) {
       const { slug } = variables
-      await queryClient.invalidateQueries({ queryKey: orgSSOKeys.orgSSOConfig(slug) })
+      await Promise.all([
+        queryClient.invalidateQueries({ queryKey: orgSSOKeys.orgSSOConfig(slug) }),
+        queryClient.invalidateQueries({ queryKey: organizationKeysV1.members(slug) }),
+      ])
       await onSuccess?.(data, variables, context)
     },
     async onError(data, variables, context) {
